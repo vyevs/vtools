a07a643b5a551707293a378eb528dddf89ddfad7
Vladimir Yevseenko <yevseenko.vladimir@gmail.com>
2024-10-21 19:09:53 -0600

    Stack and Set docs

--- set.go
+++ set.go
@@ -1,7 +1,11 @@
 package vtools
 
+// Set is a set data structure that keeps track of items.
+// A zero-value Set is not ready to use, create one using NewSet.
 type Set[T comparable] map[T]struct{}
 
+// NewSet returns a Set with the specified capacity.
+// The capacity may prevent allocations when working with the set.
 func NewSet[T comparable](cap int) Set[T] {
 	return make(map[T]struct{}, cap)
 }
@@ -10,11 +14,14 @@ func (s Set[T]) Add(t T) {
 	s[t] = struct{}{}
 }
 
+// Contains returns whether t exists in the set.
 func (s Set[T]) Contains(t T) bool {
 	_, found := s[t]
 	return found
 }
 
+// Delete removes t from the set.
+// Delete does nothing when called with an item that does not exist in the set.
 func (s Set[T]) Delete(t T) {
 	delete(s, t)
 }
---
+++ stack.go
@@ -0,0 +1,26 @@
+package vtools
+
+// Stack is a stack contianer. A zero value stack is ready to use.
+type Stack[T any] []T
+
+// NewStack returns a Stack with the specified capacity.
+// This capacity may prevent memory allocations when working with the stack.
+func NewStack[T any](cap int) Stack[T] {
+	return make([]T, 0, cap)
+}
+
+// Push puts t at the top of the stack.
+func (s *Stack[T]) Push(t T) {
+	*s = append(*s, t)
+}
+
+// Pop removes the item at the top of the stack and returns it.
+// Pop panics if called on an empty stack.
+func (s *Stack[T]) Pop() T {
+	if len(*s) == 0 {
+		panic("Pop called on empty stack")
+	}
+	t := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return t
+}
